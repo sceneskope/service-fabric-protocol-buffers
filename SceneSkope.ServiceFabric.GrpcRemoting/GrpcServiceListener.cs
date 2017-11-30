aced088cc4e6346faefc4093bde5e278aea85663
Nick Randell <nick@nickrandell.com>
2017-11-30 10:19:59 +0000

    Allow a customised endpoint name

--- SceneSkope.ServiceFabric.GrpcRemoting/GrpcServiceListener.cs
+++ SceneSkope.ServiceFabric.GrpcRemoting/GrpcServiceListener.cs
@@ -21,11 +21,15 @@ namespace SceneSkope.ServiceFabric.GrpcRemoting
 
         public ILogger Log { get; }
 
-        public GrpcServiceListener(StatefulServiceContext context, ILogger logger, IEnumerable<ServerServiceDefinition> services)
+        public string EndpointName { get; }
+
+        public GrpcServiceListener(StatefulServiceContext context, ILogger logger, IEnumerable<ServerServiceDefinition> services
+            , string endpointName = "GrpcServiceEndpoint")
         {
             Context = context;
             Log = logger;
             Services = services;
+            EndpointName = endpointName;
         }
 
         public void Abort()
@@ -44,7 +48,7 @@ namespace SceneSkope.ServiceFabric.GrpcRemoting
 
         public async Task<string> OpenAsync(CancellationToken cancellationToken)
         {
-            var serviceEndpoint = Context.CodePackageActivationContext.GetEndpoint("GrpcServiceEndpoint");
+            var serviceEndpoint = Context.CodePackageActivationContext.GetEndpoint(EndpointName);
             var port = serviceEndpoint.Port;
             var host = FabricRuntime.GetNodeContext().IPAddressOrFQDN;
 
