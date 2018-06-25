2e97a1d577f7112575a87f3ca940bf4b3effe1ea
Nick Randell <nick.randell@sceneskope.com>
2018-06-25 14:19:46 +0100

    Refactor to build better and support non service fabric projects

---
+++ SceneSkope.ProtocolBuffers/ProtobufSerializationHelpers.cs
@@ -0,0 +1,34 @@
+﻿using Google.Protobuf;
+using System;
+
+namespace SceneSkope.ProtocolBuffers
+{
+    public static class ProtobufSerializationHelpers
+    {
+        public static ArraySegment<byte> SerializeTo<T>(T message, byte[] buffer)
+            where T : IMessage<T>
+        {
+            var stream = new CodedOutputStream(buffer);
+            message.WriteTo(stream);
+            return new ArraySegment<byte>(buffer, 0, (int)stream.Position);
+        }
+
+        public static bool TryDeserialize<T>(ArraySegment<byte> buffer, out T value, out Exception exception)
+            where T : IMessage<T>, new()
+        {
+            try
+            {
+                value = new T();
+                value.MergeFrom(buffer.Array, buffer.Offset, buffer.Count);
+                exception = default;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                value = default;
+                exception = ex;
+                return false;
+            }
+        }
+    }
+}
